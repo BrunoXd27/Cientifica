3a2fc1e9ad41e770240cc7e3bd45abecd6d439cf
bruno <bruno@DESKTOP-R5TJRT7>
2024-10-12 15:28:38 -0600

    Agregar archivos de proyecto.

---
+++ Cal_cienti/Form1.Designer.cs
@@ -0,0 +1,516 @@
+﻿namespace Cal_cienti
+{
+    partial class Cal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ope = new TextBox();
+            reg = new TextBox();
+            todo = new Button();
+            borrar = new Button();
+            masmenos = new Button();
+            elin = new Button();
+            sin = new Button();
+            cos = new Button();
+            tan = new Button();
+            No_7 = new Button();
+            No_8 = new Button();
+            No_9 = new Button();
+            div = new Button();
+            root = new Button();
+            exponente = new Button();
+            cubo = new Button();
+            cuadrado = new Button();
+            No_4 = new Button();
+            No_5 = new Button();
+            No_6 = new Button();
+            por = new Button();
+            porce = new Button();
+            pi = new Button();
+            cubo_raiz = new Button();
+            No_1 = new Button();
+            No_2 = new Button();
+            No_3 = new Button();
+            menos = new Button();
+            igual = new Button();
+            expo10 = new Button();
+            exp = new Button();
+            log = new Button();
+            No_0 = new Button();
+            punto = new Button();
+            mas = new Button();
+            SuspendLayout();
+            // 
+            // ope
+            // 
+            ope.BackColor = SystemColors.Window;
+            ope.Font = new Font("Segoe UI", 18F);
+            ope.ForeColor = SystemColors.ControlText;
+            ope.Location = new Point(12, 56);
+            ope.Multiline = true;
+            ope.Name = "ope";
+            ope.Size = new Size(443, 50);
+            ope.TabIndex = 0;
+            ope.Text = "0";
+            ope.TextAlign = HorizontalAlignment.Right;
+            // 
+            // reg
+            // 
+            reg.BackColor = Color.AliceBlue;
+            reg.Font = new Font("Segoe UI", 18F);
+            reg.ForeColor = Color.SkyBlue;
+            reg.Location = new Point(12, 12);
+            reg.Multiline = true;
+            reg.Name = "reg";
+            reg.ReadOnly = true;
+            reg.Size = new Size(443, 50);
+            reg.TabIndex = 1;
+            reg.TextAlign = HorizontalAlignment.Right;
+            // 
+            // todo
+            // 
+            todo.Font = new Font("Segoe UI", 15F);
+            todo.ForeColor = Color.Crimson;
+            todo.Location = new Point(13, 121);
+            todo.Name = "todo";
+            todo.Size = new Size(274, 50);
+            todo.TabIndex = 2;
+            todo.Text = "CE";
+            todo.UseVisualStyleBackColor = true;
+            todo.Click += todo_Click;
+            // 
+            // borrar
+            // 
+            borrar.Font = new Font("Segoe UI", 15F);
+            borrar.ForeColor = Color.Crimson;
+            borrar.Location = new Point(13, 177);
+            borrar.Name = "borrar";
+            borrar.Size = new Size(218, 50);
+            borrar.TabIndex = 3;
+            borrar.Text = "C";
+            borrar.UseVisualStyleBackColor = true;
+            borrar.Click += borrar_Click;
+            // 
+            // masmenos
+            // 
+            masmenos.Location = new Point(237, 177);
+            masmenos.Name = "masmenos";
+            masmenos.Size = new Size(50, 50);
+            masmenos.TabIndex = 4;
+            masmenos.Text = "±";
+            masmenos.UseVisualStyleBackColor = true;
+            masmenos.Click += masmenos_Click;
+            // 
+            // elin
+            // 
+            elin.Font = new Font("Segoe UI", 20F);
+            elin.Location = new Point(293, 121);
+            elin.Name = "elin";
+            elin.Size = new Size(162, 106);
+            elin.TabIndex = 5;
+            elin.Text = "←";
+            elin.UseVisualStyleBackColor = true;
+            elin.Click += elin_Click;
+            // 
+            // sin
+            // 
+            sin.Location = new Point(13, 233);
+            sin.Name = "sin";
+            sin.Size = new Size(50, 50);
+            sin.TabIndex = 6;
+            sin.Text = "sin";
+            sin.UseVisualStyleBackColor = true;
+            sin.Click += sin_Click;
+            // 
+            // cos
+            // 
+            cos.Location = new Point(69, 233);
+            cos.Name = "cos";
+            cos.Size = new Size(50, 50);
+            cos.TabIndex = 7;
+            cos.Text = "cos";
+            cos.UseVisualStyleBackColor = true;
+            cos.Click += cos_Click;
+            // 
+            // tan
+            // 
+            tan.Location = new Point(125, 233);
+            tan.Name = "tan";
+            tan.Size = new Size(50, 50);
+            tan.TabIndex = 8;
+            tan.Text = "tan";
+            tan.UseVisualStyleBackColor = true;
+            tan.Click += tan_Click;
+            // 
+            // No_7
+            // 
+            No_7.Location = new Point(181, 233);
+            No_7.Name = "No_7";
+            No_7.Size = new Size(50, 50);
+            No_7.TabIndex = 9;
+            No_7.Text = "7";
+            No_7.UseVisualStyleBackColor = true;
+            No_7.Click += No_7_Click;
+            // 
+            // No_8
+            // 
+            No_8.Location = new Point(237, 233);
+            No_8.Name = "No_8";
+            No_8.Size = new Size(50, 50);
+            No_8.TabIndex = 10;
+            No_8.Text = "8";
+            No_8.UseVisualStyleBackColor = true;
+            No_8.Click += No_8_Click;
+            // 
+            // No_9
+            // 
+            No_9.Location = new Point(293, 233);
+            No_9.Name = "No_9";
+            No_9.Size = new Size(50, 50);
+            No_9.TabIndex = 11;
+            No_9.Text = "9";
+            No_9.UseVisualStyleBackColor = true;
+            No_9.Click += No_9_Click;
+            // 
+            // div
+            // 
+            div.Location = new Point(349, 233);
+            div.Name = "div";
+            div.Size = new Size(50, 50);
+            div.TabIndex = 12;
+            div.Text = "÷";
+            div.UseVisualStyleBackColor = true;
+            div.Click += div_Click;
+            // 
+            // root
+            // 
+            root.Location = new Point(405, 233);
+            root.Name = "root";
+            root.Size = new Size(50, 50);
+            root.TabIndex = 13;
+            root.Text = "√";
+            root.UseVisualStyleBackColor = true;
+            root.Click += root_Click;
+            // 
+            // exponente
+            // 
+            exponente.Location = new Point(13, 289);
+            exponente.Name = "exponente";
+            exponente.Size = new Size(50, 50);
+            exponente.TabIndex = 14;
+            exponente.Text = "x^y";
+            exponente.UseVisualStyleBackColor = true;
+            exponente.Click += exponente_Click;
+            // 
+            // cubo
+            // 
+            cubo.Location = new Point(69, 289);
+            cubo.Name = "cubo";
+            cubo.Size = new Size(50, 50);
+            cubo.TabIndex = 15;
+            cubo.Text = "x^3";
+            cubo.UseVisualStyleBackColor = true;
+            cubo.Click += cubo_Click;
+            // 
+            // cuadrado
+            // 
+            cuadrado.Location = new Point(125, 289);
+            cuadrado.Name = "cuadrado";
+            cuadrado.Size = new Size(50, 50);
+            cuadrado.TabIndex = 16;
+            cuadrado.Text = "x^2";
+            cuadrado.UseVisualStyleBackColor = true;
+            cuadrado.Click += cuadrado_Click;
+            // 
+            // No_4
+            // 
+            No_4.Location = new Point(181, 289);
+            No_4.Name = "No_4";
+            No_4.Size = new Size(50, 50);
+            No_4.TabIndex = 17;
+            No_4.Text = "4";
+            No_4.UseVisualStyleBackColor = true;
+            No_4.Click += No_4_Click;
+            // 
+            // No_5
+            // 
+            No_5.Location = new Point(237, 289);
+            No_5.Name = "No_5";
+            No_5.Size = new Size(50, 50);
+            No_5.TabIndex = 18;
+            No_5.Text = "5";
+            No_5.UseVisualStyleBackColor = true;
+            No_5.Click += No_5_Click;
+            // 
+            // No_6
+            // 
+            No_6.Location = new Point(293, 289);
+            No_6.Name = "No_6";
+            No_6.Size = new Size(50, 50);
+            No_6.TabIndex = 19;
+            No_6.Text = "6";
+            No_6.UseVisualStyleBackColor = true;
+            No_6.Click += No_6_Click;
+            // 
+            // por
+            // 
+            por.Location = new Point(349, 289);
+            por.Name = "por";
+            por.RightToLeft = RightToLeft.No;
+            por.Size = new Size(50, 50);
+            por.TabIndex = 20;
+            por.Text = "*";
+            por.UseVisualStyleBackColor = true;
+            por.Click += por_Click;
+            // 
+            // porce
+            // 
+            porce.Location = new Point(405, 289);
+            porce.Name = "porce";
+            porce.Size = new Size(50, 50);
+            porce.TabIndex = 21;
+            porce.Text = "%";
+            porce.UseVisualStyleBackColor = true;
+            porce.Click += porce_Click;
+            // 
+            // pi
+            // 
+            pi.Location = new Point(13, 345);
+            pi.Name = "pi";
+            pi.Size = new Size(81, 50);
+            pi.TabIndex = 22;
+            pi.Text = "(π)";
+            pi.UseVisualStyleBackColor = true;
+            pi.Click += pi_Click;
+            // 
+            // cubo_raiz
+            // 
+            cubo_raiz.Location = new Point(100, 345);
+            cubo_raiz.Name = "cubo_raiz";
+            cubo_raiz.RightToLeft = RightToLeft.No;
+            cubo_raiz.Size = new Size(75, 50);
+            cubo_raiz.TabIndex = 23;
+            cubo_raiz.Text = "3√x";
+            cubo_raiz.UseVisualStyleBackColor = true;
+            cubo_raiz.Click += cubo_raiz_Click;
+            // 
+            // No_1
+            // 
+            No_1.Location = new Point(181, 345);
+            No_1.Name = "No_1";
+            No_1.Size = new Size(50, 50);
+            No_1.TabIndex = 25;
+            No_1.Text = "1";
+            No_1.UseVisualStyleBackColor = true;
+            No_1.Click += No_1_Click;
+            // 
+            // No_2
+            // 
+            No_2.Location = new Point(237, 345);
+            No_2.Name = "No_2";
+            No_2.Size = new Size(50, 50);
+            No_2.TabIndex = 26;
+            No_2.Text = "2";
+            No_2.UseVisualStyleBackColor = true;
+            No_2.Click += No_2_Click;
+            // 
+            // No_3
+            // 
+            No_3.Location = new Point(293, 345);
+            No_3.Name = "No_3";
+            No_3.Size = new Size(50, 50);
+            No_3.TabIndex = 27;
+            No_3.Text = "3";
+            No_3.UseVisualStyleBackColor = true;
+            No_3.Click += No_3_Click;
+            // 
+            // menos
+            // 
+            menos.Location = new Point(349, 345);
+            menos.Name = "menos";
+            menos.Size = new Size(50, 50);
+            menos.TabIndex = 28;
+            menos.Text = "-";
+            menos.UseVisualStyleBackColor = true;
+            menos.Click += menos_Click;
+            // 
+            // igual
+            // 
+            igual.Location = new Point(405, 345);
+            igual.Name = "igual";
+            igual.Size = new Size(50, 104);
+            igual.TabIndex = 29;
+            igual.Text = "=";
+            igual.UseVisualStyleBackColor = true;
+            igual.Click += igual_Click;
+            // 
+            // expo10
+            // 
+            expo10.Location = new Point(12, 401);
+            expo10.Name = "expo10";
+            expo10.Size = new Size(50, 50);
+            expo10.TabIndex = 30;
+            expo10.Text = "10^x";
+            expo10.UseVisualStyleBackColor = true;
+            expo10.Click += expo10_Click;
+            // 
+            // exp
+            // 
+            exp.Location = new Point(69, 401);
+            exp.Name = "exp";
+            exp.Size = new Size(50, 50);
+            exp.TabIndex = 31;
+            exp.Text = "EXP";
+            exp.UseVisualStyleBackColor = true;
+            exp.Click += exp_Click;
+            // 
+            // log
+            // 
+            log.Location = new Point(125, 399);
+            log.Name = "log";
+            log.Size = new Size(50, 50);
+            log.TabIndex = 32;
+            log.Text = "LOG";
+            log.UseVisualStyleBackColor = true;
+            log.Click += log_Click;
+            // 
+            // No_0
+            // 
+            No_0.Location = new Point(181, 401);
+            No_0.Name = "No_0";
+            No_0.Size = new Size(106, 50);
+            No_0.TabIndex = 33;
+            No_0.Text = "0";
+            No_0.UseVisualStyleBackColor = true;
+            No_0.Click += No_0_Click;
+            // 
+            // punto
+            // 
+            punto.Location = new Point(293, 399);
+            punto.Name = "punto";
+            punto.Size = new Size(50, 50);
+            punto.TabIndex = 34;
+            punto.Text = ".";
+            punto.UseVisualStyleBackColor = true;
+            punto.Click += punto_Click;
+            // 
+            // mas
+            // 
+            mas.Location = new Point(349, 399);
+            mas.Name = "mas";
+            mas.Size = new Size(50, 50);
+            mas.TabIndex = 35;
+            mas.Text = "+";
+            mas.UseVisualStyleBackColor = true;
+            mas.Click += mas_Click;
+            // 
+            // Cal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 461);
+            Controls.Add(mas);
+            Controls.Add(punto);
+            Controls.Add(No_0);
+            Controls.Add(log);
+            Controls.Add(exp);
+            Controls.Add(expo10);
+            Controls.Add(igual);
+            Controls.Add(menos);
+            Controls.Add(No_3);
+            Controls.Add(No_2);
+            Controls.Add(No_1);
+            Controls.Add(cubo_raiz);
+            Controls.Add(pi);
+            Controls.Add(porce);
+            Controls.Add(por);
+            Controls.Add(No_6);
+            Controls.Add(No_5);
+            Controls.Add(No_4);
+            Controls.Add(cuadrado);
+            Controls.Add(cubo);
+            Controls.Add(exponente);
+            Controls.Add(root);
+            Controls.Add(div);
+            Controls.Add(No_9);
+            Controls.Add(No_8);
+            Controls.Add(No_7);
+            Controls.Add(tan);
+            Controls.Add(cos);
+            Controls.Add(sin);
+            Controls.Add(elin);
+            Controls.Add(masmenos);
+            Controls.Add(borrar);
+            Controls.Add(todo);
+            Controls.Add(reg);
+            Controls.Add(ope);
+            Name = "Cal";
+            RightToLeft = RightToLeft.No;
+            Text = "Calculadora científica";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox ope;
+        private TextBox reg;
+        private Button todo;
+        private Button borrar;
+        private Button masmenos;
+        private Button elin;
+        private Button sin;
+        private Button cos;
+        private Button tan;
+        private Button No_7;
+        private Button No_8;
+        private Button No_9;
+        private Button div;
+        private Button root;
+        private Button exponente;
+        private Button cubo;
+        private Button cuadrado;
+        private Button No_4;
+        private Button No_5;
+        private Button No_6;
+        private Button por;
+        private Button porce;
+        private Button pi;
+        private Button cubo_raiz;
+        private Button No_1;
+        private Button No_2;
+        private Button No_3;
+        private Button menos;
+        private Button igual;
+        private Button expo10;
+        private Button exp;
+        private Button log;
+        private Button No_0;
+        private Button punto;
+        private Button mas;
+    }
+}
---
+++ Cal_cienti/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using NCalc;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Cal_cienti
+{
+    public partial class Cal : Form
+    {
+        public Cal()
+        {
+            InitializeComponent();
+        }
+
+        public double EvaluateExpression(string expression)
+        {
+            expression = Exponente(expression);
+            expression = ReplaceSpecialSymbols(expression);
+
+            var e = new NCalc.Expression(expression);
+            return Convert.ToDouble(e.Evaluate());
+        }
+
+        private string ReplaceSpecialSymbols(string expression)
+        {
+            expression = Regex.Replace(expression, @"\s*√\s*(\d+)", "Sqrt($1)");
+            expression = Regex.Replace(expression, @"√\s*(\d+)", "Sqrt($1)");
+            return expression;
+        }
+
+        private string Exponente(string expression)
+        {
+            expression = ope.Text;
+
+            if (expression.Contains('^'))
+            {
+                string[] partes = expression.Split('^');
+                if (partes.Length == 2 && double.TryParse(partes[0], out double baseNumero) && double.TryParse(partes[1], out double exponente))
+                {
+                    double resultado = Math.Pow(baseNumero, exponente);
+                    return resultado.ToString();
+                }
+            }
+
+            if (expression.Contains('√'))
+            {
+                string[] partes = expression.Split('√');
+                if (partes.Length == 2 && double.TryParse(partes[0], out double exponente) && double.TryParse(partes[1], out double baseNumero))
+                {
+                    double resultado = Math.Pow(baseNumero, 1.0 / exponente);
+                    return resultado.ToString();
+                }
+            }
+            if (expression.Contains('π'))
+            {
+                // Reemplaza π por el valor de Pi
+                expression = expression.Replace("π", Math.PI.ToString(CultureInfo.InvariantCulture));
+                return expression;
+            }
+            return expression;
+        }
+
+        private void No_0_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "0";
+        }
+
+        private void No_1_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "1";
+        }
+
+        private void No_2_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "2";
+        }
+
+        private void No_3_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "3";
+        }
+
+        private void No_4_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "4";
+        }
+
+        private void No_5_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "5";
+        }
+
+        private void No_6_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "6";
+        }
+
+        private void No_7_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "7";
+        }
+
+        private void No_8_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "8";
+        }
+
+        private void No_9_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "9";
+        }
+
+        private void punto_Click(object sender, EventArgs e)
+        {
+            ope.Text = ope.Text + ".";
+        }
+
+        private void mas_Click(object sender, EventArgs e)
+        {
+            if (ope.Text != "0") { ope.Text = ope.Text + "+"; }
+        }
+
+        private void menos_Click(object sender, EventArgs e)
+        {
+            if (ope.Text != "0") { ope.Text = ope.Text + "-"; }
+        }
+
+        private void por_Click(object sender, EventArgs e)
+        {
+            if (ope.Text != "0") { ope.Text = ope.Text + "*"; }
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            if (ope.Text != "0") { ope.Text = ope.Text + "/"; }
+        }
+
+        private void root_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "√";
+        }
+
+        private void porce_Click(object sender, EventArgs e)
+        {
+            if (ope.Text != "0")
+            {
+                reg.Text = ope.Text + "%";
+                string expression = ope.Text;
+                double result = EvaluateExpression(expression) / 100;
+                ope.Text = result.ToString();
+            }
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            reg.Text = ope.Text;
+            reg.Text = reg.Text + "=";
+            string expression = ope.Text;
+            var result = EvaluateExpression(expression);
+            ope.Text = result.ToString();
+        }
+
+        private void todo_Click(object sender, EventArgs e)
+        {
+            ope.Text = "0";
+            reg.Text = "";
+        }
+
+        private void borrar_Click(object sender, EventArgs e)
+        {
+            ope.Text = "0";
+        }
+
+        private void elin_Click(object sender, EventArgs e)
+        {
+            if (ope.TextLength == 1) { ope.Text = "0"; }
+            else { ope.Text = ope.Text.Substring(0, ope.Text.Length - 1); }
+        }
+
+        private void ope_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && "+-*/%^√π±".IndexOf(e.KeyChar) == -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void masmenos_Click(object sender, EventArgs e)
+        {
+            char primerCaracter = ope.Text[0];
+            string texto = ope.Text;
+            if (primerCaracter == '-')
+            {
+                ope.Text = texto.Substring(1);
+            }
+            else
+            {
+                ope.Text = "-" + ope.Text;
+            }
+        }
+
+        private void pi_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "π";
+        }
+
+        private void cuadrado_Click(object sender, EventArgs e)
+        {
+            ope.Text = ope.Text + "^2";
+        }
+
+        private void cubo_Click(object sender, EventArgs e)
+        {
+            ope.Text = ope.Text + "^3";
+        }
+
+        private void exponente_Click(object sender, EventArgs e)
+        {
+            ope.Text = ope.Text + "^";
+        }
+
+        private void expo10_Click(object sender, EventArgs e)
+        {
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "10^";
+        }
+
+        private void exp_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double numero))
+            {
+                double exponente = Math.Exp(numero);
+                reg.Text = ope.Text;
+                ope.Text = exponente.ToString();
+            }
+        }
+
+        private void log_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double numero))
+            {
+                double logaritmo = Math.Log10(numero);
+                ope.Text = logaritmo.ToString();
+            }
+        }
+
+        private void cubo_raiz_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double numero))
+            {
+                double raizCubica = Math.Pow(numero, 1.0 / 3.0);
+                reg.Text = ope.Text;
+                ope.Text = raizCubica.ToString();
+            }
+        }
+
+        private void raiz_Click(object sender, EventArgs e)
+        {
+            //Si se quiere usar basta con poner "7√50"
+            if (ope.Text == "0") { ope.Text = ""; }
+            ope.Text = ope.Text + "√";
+        }
+
+        private void sin_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double angulo))
+            {
+                double seno = Math.Sin(angulo * Math.PI / 180);
+                ope.Text = seno.ToString();
+            }
+        }
+
+        private void cos_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double angulo))
+            {
+                double coseno = Math.Cos(angulo * Math.PI / 180);
+                ope.Text = coseno.ToString();
+            }
+        }
+
+        private void tan_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(ope.Text, out double angulo))
+            {
+                double tangente = Math.Tan(angulo * Math.PI / 180);
+                ope.Text = tangente.ToString();
+            }
+        }
+    }
+}
